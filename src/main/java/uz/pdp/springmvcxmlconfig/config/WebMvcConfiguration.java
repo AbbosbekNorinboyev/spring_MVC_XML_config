8f5f344ca37564339b608a72deb7c54fbb94bb42
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2024-09-21 18:45:15 +0500

    SPRING MVC WITH XML CONFIG

---
+++ src/main/java/uz/pdp/springmvcxmlconfig/config/WebMvcConfiguration.java
@@ -0,0 +1,11 @@
+package uz.pdp.springmvcxmlconfig.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+}
